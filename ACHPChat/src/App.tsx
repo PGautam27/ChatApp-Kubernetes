8453cfb5b84233cee1ff6936190ab2774d5380d3
PGautam27 <gautamappu0@gmail.com>
2024-10-24 16:13:50 +0530

    updated the full service dns name in web

--- ACHPChat/src/App.tsx
+++ ACHPChat/src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useRef } from 'react'
 import { io } from 'socket.io-client';
 
-const socket = io('http://achpserver-service:4000');
+const socket = io('http://chat-backend.default.svc.cluster.local:4000');
 // const socket = io('localhost:4000')
 
 function App() {
